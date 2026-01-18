6a57fd22c999c9ef31571d7572a920d9fbf39ff3
Carles Bou <cbou@zat.net>
2026-01-18 21:11:05 +0100

    Creació metodes i classes per als prodcutes

--- products/product.py
+++ products/product.py
@@ -1,38 +1,93 @@
 from abc import ABC, abstractmethod
-#Write your code here
+from products.food_package import FoodPackage, Wrapping, Bottle, Glass, Box
+
 
 class Product(ABC):
-    def __init__(self,id:str,name:str,price:float):
+
+    def __init__(self, id:str, name:str, price:float):
       self.id = id
       self.name = name
-      self.price = price     
-    
-    def describe(self):
-        return f"Product - Type: {self.type()}, Name: {self.name}, Id: {self.id} , Price: {self.price} , {self.foodPackage().describe()}."   
-    
+      self.price = price
+
+
     @abstractmethod
     def type(self) -> str:
         pass
+
+
     @abstractmethod
     def foodPackage(self)->FoodPackage:
-        pass  
+        pass
+
+
+    def describe(self):
+        return f"Product - Type: {self.type()}, Name: {self.name}, Id: {self.id} , Price: {self.price} , {self.foodPackage().describe()}."   
+
+
 
 class Hamburger(Product):
-    def __init__(self,id:str,name:str,price:float):
-        super().__init__(id,name,price)
+    """
+    Implementació de la classe Hamburger que hereta de Product.
+    Representa una hamburguesa amb el seu tipus i embolcall.
+    """
+
+    def __init__(self, id:str, name:str, price:float):
+        super().__init__(id, name, price)
+
+
     def type(self) -> str:
         return "Hamburguesa"
+    
+
     def foodPackage(self) -> FoodPackage:
         return Wrapping()
         
+
+
 class Soda(Product):
-    #Write your code here
-    pass
+    """
+    Representa una beguda tipus soda.
+    """
+
+    def __init__(self, id:str, name:str, price:float):
+        super().__init__(id, name, price)
+
+
+    def type(self) -> str:
+        return "Soda"
+
+
+    def foodPackage(self) -> FoodPackage:
+        return Bottle()
+
+
 
 class Drink(Product):
-    #Write your code here
-    pass
+    """
+    Representa una beguda genèrica.
+    """
+
+    def __init__(self, id:str, name:str, price:float):
+        super().__init__(id, name, price)
+
+    def type(self) -> str:
+        return "Drink"
+    
+    def foodPackage(self) -> FoodPackage:
+        return Glass()
+
+
 
 class HappyMeal(Product):
-    #Write your code here
-    pass
+    """
+    Representa un Happy Meal.
+    """
+
+    def __init__(self, id:str, name:str, price:float):
+        super().__init__(id, name, price)
+
+    def type(self) -> str:
+        return "Happy Meal"
+    
+    def foodPackage(self) -> FoodPackage:
+        return Box()
