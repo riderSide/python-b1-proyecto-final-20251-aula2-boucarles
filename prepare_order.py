e4d6e9047bafbff113add7bd0991bf444c12b9c5
Carles Bou <cbou@zat.net>
2026-01-19 23:47:22 +0100

    creació del fitxer principal de la applicació

--- prepare_order.py
+++ prepare_order.py
@@ -1,111 +1,230 @@
-"""
-Ejercicio 1: Sistema de comida rápida
- 
-Implementar un paquete llamado ‘products' que tiene dos módulos: ‘food_package.py' y ‘product.py', con la siguiente estructura:
+import os
 
-products/
-        __init__.py
-        food_package.py
-        product.py
+from users import Customer, Cashier
+from util import CSVFileManager, CustomerConverter, CashierConverter, ProductConverter
 
-El módulo food_package.py contendrá una clase abstracta denominada 'FoodPackage' con dos funciones abstractas: 'def pack(self)  -> str ' y 'def material(self) -> str '. Esta clase nos permite crear un tipo específico de paquete o envoltura dependiendo del tipo de alimento a empacar, por ejemplo:
+from util.drawer import Drawer
 
-Un vaso de soda puede ser empacado en un paquete tipo vaso y el material puede ser cartón. 
-Una hamburguesa puede ser empacada en un paquete tipo envoltura de papel y el material puede ser aluminio.
+from orders import Order
 
-En el mismo módulo se deberán incluir las implementaciones concretas para cada una de las siguientes clases ‘Wrapping’, ‘Bottle’, ‘Glass’ y ‘Box’, es decir, estas deben implementar los métodos anteriores y devolver un valor. Por ejemplo, la clase ‘Wrapping’ se puede definir como:
+# definm la constant de l'amplada de la pantalla
+SCREEN_WIDTH = 60
 
-class Wrapping(FoodPackage):  
-  def pack(self):
-    return "Food Wrap Paper"
-  def material(self):
-    return "Aluminium" 
-
-El módulo 'product.py’ contendrá una clase abstracta denominada 'Product' con dos funciones abstractas: 'def type(self) -> str' y 'def foodPackage(self)-> FoodPackage. Esta clase nos permita crear un producto específico y relacionarlo con su tipo de empaque por ejemplo:
-
-Un producto con código de barras G1, es una soda Sprite cuyo precio es de 5 euros, pertenece al tipo Soda y puede ser empacado en un paquete tipo vaso y el material puede ser cartón. 
-Un producto con código de barras H1, es una hamburguesa Bacon  cuyo precio es de 15 euros, pertenece al tipo Hamburger y puede ser empacado en un paquete un paquete tipo envoltura de papel y el material puede ser aluminio.
-
-En el mismo módulo se deberán incluir las implementaciones concretas para cada una de las clases ‘Hamburger’, ‘Soda’, ‘Drink’ y ‘HappyMeal’, es decir, de forma parecida al módulo anterior, estas deben implementar los métodos anteriores y devolver un valor. Por ejemplo, la clase ‘Hamburger’, se puede definir como:
-
-class Hamburger(Product):
-    def __init__(self, id:str, name:str, price:float):
-        super().__init__(id,name,price)
-    def type(self) -> str:
-        return "Hamburger"
-    def foodPackage(self) -> FoodPackage:
-        return Wrapping()
-        
-Implementar un paquete llamado ‘users' que tiene un módulo ‘user.py', con la siguiente estructura:
-
-users/
-        __init__.py
-        user.py
-
-El módulo 'user.py' contendrá una clase abstracta denominada ‘User’ que tiene un constructor por defecto para los siguientes datos 'def __init__(self, dni:str, name:str, age:int) ', con una función abstracta: 'def describe(self) '.
-
-Luego en el mismo módulo se deberán incluir las implementaciones concretas para cada una de las clases ‘Cashier’ y ‘Customer’, es decir, estas deben implementar los métodos anteriores y devolver un valor. Adicionalmente, estas clases se diferencian por los parámetros que reciben sus constructores, por tanto, debemos hacer uso de herencia para inicializar el constructor de la clase padre y agregar características propias a cada clase.  
-
-Implementar un paquete llamado 'util' que tiene dos módulos, denominados 'file_manager.py' y 'converter.py’, con la siguiente estructura:
-
-util/
-        __init__.py
-        file_manager.py
-        converter.py
-
-El módulo ‘file_manager.py' contendrá una clase ‘CSVFileManager’ la cual es una implementaciòn libre y debe incluir las funciones:
-
-La función 'def read(self)' lee un archivo en formato CSV y permite exportar su resultado como un Data Frame.
-La función 'def write(self, dataFrame)' convierte un Data Frame en un archivo CSV. Esta es una función opcional, se deja al estudiante la implementación.
-
-Los archivos en formato CSV se encuentran en la ruta “data/”, a continuación, se describe el contenido de cada archivo:
-
-cashiers.csv: Información de los cajeros que harán uso del sistema.
-customers.csv: Información de los clientes que harán uso del sistema.
-drinks.csv: Información de los diferentes tipos de bebidas.
-sodas.csv: Información de los diferentes tipos de gaseosas.
-hamburgers.csv: Información de los diferentes tipos de hamburguesas.
-happyMeal.csv: Información de los diferentes tipos de happy meals.
-
-El módulo 'converter.py' contendrá una clase denominada ‘Converter’ con una función abstracta para convertir las filas de un Data Frame en instancias de objetos. La función sería ‘def convert(self, dataFrame, *args) -> list’. Adicionalmente esta clase debe incluir un método que permite imprimir la información de los objetos ‘def print(self, list)’. En el mismo módulo se deberán incluir las implementaciones específicas que permitan leer los archivos en formato CSV y convertir sus filas en objetos de cada clase utilizando los paquetes product y users.
-
-Implementar un paquete llamado 'orders' que tiene un módulo 'order.py', con la siguiente estructura:
-
-orders/
-        __init__.py
-        order.py
-
-El módulo 'order.py' contendrá una clase denominada ‘Order’ con un constructor ‘def __init__(self, cashier:Cashier, customer:Customer):’, el cual permite inicializar la clase con los datos del cajero, del cliente y la lista de productos vacía por defecto. Además, debe incluir tres funciones para agregar productos, calcular el total de la orden solicitada y mostrar la información de la orden que está siendo procesada. Las funciones son ‘def add(self, product: Product)', ' def calculateTotal(self) -> float' y ‘def show(self)’, respectivamente.
-
-Finalmente tendremos una clase principal que se llamará ‘PrepareOrder’ en la cual se deberá realizar una implementación que permita integrar los diferentes módulos empleados para leer los archivos en formato CSV y convertirlos en objetos. La implementación de esta clase es libre, es decir, no indicaremos las funciones que debe contener, pero la funcionalidad de la clase debe permitir crear una opción de menú que permita buscar los clientes, los cajeros y los productos para finalmente crear una orden. 
-
-Se sugiere utilizar los métodos de entrada de teclado para leer los datos del dni cajero, cliente e id de los productos. 
-
-
-A grandes rasgos, la aplicación seguiría los siguientes pasos:
-
-1)	Leer archivos en formato csv: 
-a.	Leer cada archivo en formato csv: Utilizar una instancia de la clase 'CSVFileManager' y llamar al método 'read()'.
-2)	Convertir a listas de objetos:
-a.	Convertir cajeros: Función creada por el alumno  
-b.	Convertir clientes: Función creada por el alumno 
-c.	Convertir productos: Función creada por el alumno 
-3)	Preparar Orden:
-a.	Buscar cajero por dni: Función creada por el alumno y debe devolver una instancia de tipo cajero.
-b.	Buscar cliente por dni. Función creada por el alumno y debe devolver una instancia de tipo cliente.
-c.	Inicializar Orden: Utilizar una instancia la clase 'Order', e inicializar con su constructor por defecto.
-d.	Mostrar productos a vender: Función creada por el alumno.
-e.	Escoger productos: Función creada por el alumno.
-f.	Agregar productos: Utilizar la instancia la clase 'Order', del paso c y llamar al método 'add()'.
-4)	Mostrar Orden: Utilizar la instancia la clase 'Order', del paso c y llamar al método 'show()'
-
-
-"""
-#Write your code here
-from users import *
-
-    
 class PrepareOrder:
- #Write your code here
- pass
 
+    def __init__(self):
+        self.customers= []
+        self.cashiers= []
+        self.products= []
+        self.order= []
+        
+        if not self.read_data_soources():
+            print("Error en carregar les dades inicials dels fitxers CSV.")
+            exit(1)
+        self.run()
+
+
+    def run(self):
+        drawer = Drawer(SCREEN_WIDTH)
+
+        options = {
+            "0": "Nova comanda",
+            "1": "Llistar clients",
+            "2": "Llistar caixers",
+            "3": "Llistar productes",
+            "9": "Sortir"
+        }
+
+        while True:
+            choice = drawer.main_menu(options)
+            match choice:
+                case '0':
+                    drawer.modal(text= "Iniciant nova comanda...", timeout= 1)
+                    self.new_order(drawer)
+                case '1':
+                    drawer.list("Llistat de Clients", self.customers)
+
+                case '2':
+                    drawer.list("Llistat de Caixers", self.cashiers)
+
+                case '3':
+                    drawer.list("Llistat de Productes", self.products)
+
+                case '9':
+                    drawer.modal("Sortint del sistema. Fins aviat!")
+                    exit(0)
+
+
+    def cashier_login(self, drawer: Drawer) -> Cashier:
+        """
+        Sol·licita al caixer que introdueixi el seu DNI i verifica si existeix.
+        Args:
+            drawer (Drawer): instància de la classe Drawer per gestionar la interfície d'usuari
+        Returns:
+            Cashier: retorna l'objecte Cashier si es troba
+        """
+        drawer.clear_screen()
+        cashier_dni = drawer.ask("Introdueix DNI del caixer: ")
+
+        # cerquem el dni a la llista
+        for cashier in self.cashiers:
+            if str(cashier.dni) == cashier_dni:
+                drawer.show_message(cashier.describe(), timeout=1)
+                return cashier
+
+        # No s'ha trobat el caixer, el torem a demanar
+        drawer.modal("Caixer no trobat.", timeout=2)
+        self.cashier_login(drawer)
+    
+
+    def search_customer(self, drawer: Drawer) -> Customer:
+        """
+        Sol·licita al client que introdueixi el seu DNI i verifica si existeix.
+        Args:
+            drawer (Drawer): instància de la classe Drawer per gestionar la interfície d'usuari
+        Returns:
+            Customer: retorna l'objecte Customer si es troba
+        """
+
+        drawer.clear_screen()
+        customer_dni = drawer.ask("Introdueix DNI del client: ")
+
+        # cerquem el dni a la llista
+        for customer in self.customers:
+            if str(customer.dni) == customer_dni:
+                drawer.show_message(customer.describe(), timeout=1)
+                return customer
+
+        # No s'ha trobat el client, el torem a demanar
+        drawer.modal("Client no trobat.", timeout=2)
+        self.search_customer(drawer)
+
+
+    def product_selection(self, drawer: Drawer, order: Order):
+        """
+        Permet seleccionar productes per afegir a la comanda.
+        Args:
+            drawer (Drawer): instància de la classe Drawer per gestionar la interfície d'usuari
+            order (Order): instància de la classe Order per afegir productes
+        """
+
+        drawer.clear_screen()
+        while True:
+            drawer.list("Llistat de Productes", self.products, return_after= False)
+            choice = drawer.ask("Selecciona un producte per afegir: ")
+            
+            found= False
+            for product in self.products:
+                if str(product.id) == choice:
+                    if not order.add(product):
+                        drawer.modal("Error en afegir el producte a la comanda.", timeout=2)
+                        return
+                    
+                    drawer.modal(f"Producte '{product.name}' afegit a la comanda.", timeout=1)
+                    found = True
+                    break
+
+            if not found:
+                drawer.modal("Producte no trobat. Torna-ho a intentar.", timeout=2)
+            else:
+                # preguntem si vol afegir més productes
+                add_more = drawer.ask("Vols afegir més productes? (s/n): ")
+                if add_more.lower() == 'n':
+                    break
+
+
+      
+    def new_order(self, drawer: Drawer):
+        """
+        Inicia el proecdiment per a geenrar una nova comanda.
+        """
+        # sol·licitar al caixer
+        cashier = self.cashier_login(drawer)
+
+        # sol·licitar al client
+        customer = self.search_customer(drawer)
+
+        # creem la comanda
+        order = Order(cashier, customer)
+
+        # afegim productes
+        self.product_selection(drawer, order)
+
+        # gaurdem la comanda
+        if not order.save():
+            drawer.modal("Error en guardar la comanda.", timeout=2)
+            return        
+        
+        # mostrem el resum de la comanda
+        drawer.show_order_summary(order)
+
+        
+
+
+
+
+
+    def read_data_soources(self) -> bool:
+        """
+        Carrega les dades inicals dels fitxers CSV i les converteix 
+        en llistes d'objectes.
+        Returns:
+            bool: True si la càrrega és exitosa, False en cas contrari.
+        """
+        try:
+            # Carregar i convertir dades dels clients
+            customer_file_manager = CSVFileManager("data/customers.csv")
+            customer_df = customer_file_manager.read()
+            customer_converter = CustomerConverter()
+            self.customers = customer_converter.convert(customer_df)
+        except Exception as e:
+            print(f"S'ha produït un error al carregar les dades dels clients: {e}")
+            return False
+
+        try:
+            # Carregar i convertir dades dels caixers
+            cashier_file_manager = CSVFileManager("data/cashiers.csv")
+            cashier_df = cashier_file_manager.read()
+            cashier_converter = CashierConverter()
+            self.cashiers = cashier_converter.convert(cashier_df)
+        except Exception as e:
+            print(f"S'ha produït un error al carregar les dades dels caixers: {e}")
+            return False
+
+
+        """
+        Creem un llista (mapping) amb els prodcutes que volem carregar i els 
+        seus fitxers corresponents.
+        Amb aquest sistema fem un bucle per carregar i convertir.  
+        Evitem repetir codi i el codi és més fácil d'ampliar.
+        """
+        try:
+            products_dict = {
+                'Hamburger': "hamburgers.csv",
+                'Soda': "sodas.csv",
+                'HappyMeal': "happyMeal.csv",
+                'Drink': "drinks.csv"
+            }
+            
+            # Carregar i convertir dades dels productes
+            for product in products_dict:
+                # generem la ruta del fitxer
+                file_name = os.path.join("data", products_dict[product])
+
+                file_manager = CSVFileManager(file_name)
+                df = file_manager.read()
+                converter = ProductConverter()
+                converted_products = converter.convert(df, product)
+
+                # afegim els productes a la llista general
+                self.products.extend(converted_products)
+
+        except Exception as e:
+            print(f"S'ha produït un error al carregar les dades inicials: {e}")
+            return False
+        return True
+
+
+if __name__ == "__main__":
+    app = PrepareOrder()
+    app.run()
